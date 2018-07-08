44309ffee6b71252c8303eddd47f4f1ca71bfee0
codesmack <akb81ster@gmail.com>
2018-07-09 02:26:48 +0530

    Boot and Gradle play toys

---
+++ src/main/java/com/codesmack/demoboot/DemoBootInitializer.java
@@ -0,0 +1,12 @@
+package com.codesmack.demoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootInitializer.class, args);
+	}
+}
---
+++ src/test/java/com/codesmack/demoboot/DemoBootInitializerTests.java
@@ -0,0 +1,16 @@
+package com.codesmack.demoboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoBootInitializerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
